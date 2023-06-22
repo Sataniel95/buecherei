4ee30d26a068402344294a78ac469a696c78c0da
Sataniel95 <125015659+Sataniel95@users.noreply.github.com>
2023-06-22 12:57:25 +0200

    Add files via upload

--- db_conn.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-from mysql.connector import Error
-conn = mysql.connector.connect(host='localhost',
-                                         database='library_management',
-                                         user='root',
-                                         password='3956')
